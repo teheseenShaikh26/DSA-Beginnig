41343dc5c735d1029d826a684496f8f4c451489b
Teheseen Shaikh <78405778+teheseenShaikh26@users.noreply.github.com>
2023-09-21 01:00:28 +0530

    Add files via upload

---
+++ Rotate bits.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+    
+    ArrayList<Integer> rotate(int N, int D)
+    {
+        // your code here
+        //code here.
+            D = D % 16;
+            ArrayList<Integer> list = new ArrayList<>();
+            
+            int l = (N << D | N >> (16 - D)) &65535;
+            int r = (N >> D | N << (16 - D)) &65535;
+            
+            list.add(l);
+            list.add(r);
+            
+            return list;
+    }
+}
